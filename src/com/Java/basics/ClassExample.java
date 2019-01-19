8503702fc6c520a696cd5f6864d1f5b44313d97c
ASC <asc@github.com>
2019-01-19 13:47:17 +0530

    print hello world

--- src/com/Java/basics/ClassExample.java
+++ src/com/Java/basics/ClassExample.java
@@ -3,8 +3,9 @@ package com.Java.basics;
 public class ClassExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Hello World");
+		
 	}
 
 }
